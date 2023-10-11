4ca8110230592387834221f8a03e551cdc863cdf
Patryk Milkiewicz <G00397265@atu.ie>
2023-10-11 10:23:43 +0100

    Created a UserController Class with a PostMapping

---
+++ src/main/java/ie/atu/userservice/UserController.java
@@ -0,0 +1,29 @@
+package ie.atu.userservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class UserController {
+    private RegistrationServiceClient registrationServiceClient;
+    //private final AcknowledgeService acknowledgeService;
+
+    @Autowired
+    public UserController(/*AcknowledgeService,*/ RegistrationServiceClient registrationServiceClient){
+        //this.acknowledgeService = acknowledgeService;
+        this.registrationServiceClient = registrationServiceClient;
+    }
+
+    @PostMapping("confirm-and-register")
+    public Map<String, String> confirmAndRegister(@RequestBody UserDetails userDetails){
+        String confirm = userDetails.getEmail();
+        Map<String, String> responseMessage = new HashMap<>();
+        responseMessage.put("Welcome, ", confirm);
+        return responseMessage;
+    }
+}
