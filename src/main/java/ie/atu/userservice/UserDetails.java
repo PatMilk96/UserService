c86ed345642c4297f0ec6c67bf502527fdac35cd
Patryk Milkiewicz <G00397265@atu.ie>
2023-10-11 10:01:03 +0100

    Created a UserDetails Class with some basic code

---
+++ src/main/java/ie/atu/userservice/UserDetails.java
@@ -0,0 +1,15 @@
+package ie.atu.userservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserDetails {
+    private String name;
+    private String email;
+}
